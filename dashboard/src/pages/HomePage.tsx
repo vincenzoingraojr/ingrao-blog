e36529653ef59936c1e0fc74fd15b320c578c133
Vincenzo Ingrao Jr <vincenzoingraojr@gmail.com>
2022-11-11 17:47:09 +0100

    first version of the dashboard is complete

--- dashboard/src/pages/HomePage.tsx
+++ dashboard/src/pages/HomePage.tsx
@@ -1,15 +1,132 @@
 import Head from "../components/Head";
 import PageLayout from "../components/layouts/PageLayout";
 import PageContentLayout from "../components/layouts/sublayouts/PageContentLayout";
+import styled from "styled-components";
+import { LoadingContainer, OptionContainer, OptionTitle, PageBlock, PageText } from "../styles/global";
+import { useDashPostFeedQuery, useDraftPostFeedQuery, usePostFeedQuery } from "../generated/graphql";
+import { Link } from "react-router-dom";
+import LoadingComponent from "../components/utils/LoadingComponent";
+import PostComponent from "../components/layouts/items/PostComponent";
+import { devices } from "../styles/devices";
+
+const HomePageContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 48px;
+`;
+
+const DashStatsContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    gap: 24px;
+
+    @media (min-width: 560px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
+`;
+
+const StatsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 18px;
+    width: 100%;
+    align-items: center;
+`;
+
+const DataContainer = styled.div`
+    display: block;
+    font-weight: 700;
+    font-size: 64px;
+`;
+
+const DataTypeContainer = styled(PageText)`
+    font-size: 16px;
+`;
+
+const PublicPostsGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    gap: 24px;
+
+    @media (min-width: 560px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media ${devices.laptopS} {
+        grid-template-columns: repeat(3, 1fr);
+    }
+`;
 
 function HomePage() {
+    const { data: dashPostsData } = useDashPostFeedQuery({ fetchPolicy: "cache-and-network" });
+    const { data, loading, error } = usePostFeedQuery({ fetchPolicy: "cache-and-network" });
+    const { data: draftPostsData } = useDraftPostFeedQuery({ fetchPolicy: "cache-and-network" });
+    
     return (
         <>
             <Head
                 title="Dashboard | ingrao.blog"
                 description="This is the dashboard homepage."
             />
-            <PageLayout content={<PageContentLayout content={<>Home.</>} />} />
+            <PageLayout content={
+                <PageContentLayout content={
+                    <HomePageContainer>
+                        <DashStatsContainer>
+                            <StatsContainer>
+                                <DataContainer>{dashPostsData?.dashPostFeed.length}</DataContainer>
+                                <DataTypeContainer>Total number of posts</DataTypeContainer>
+                            </StatsContainer>
+                            <StatsContainer>
+                                <DataContainer>{data?.postFeed.length}</DataContainer>
+                                <DataTypeContainer>Number of your public posts</DataTypeContainer>
+                            </StatsContainer>
+                            <StatsContainer>
+                                <DataContainer>{draftPostsData?.draftPostFeed.length}</DataContainer>
+                                <DataTypeContainer>Number of <Link to="/create-post" title="Your drafts" aria-label="Your drafts">your drafts</Link></DataTypeContainer>
+                            </StatsContainer>
+                        </DashStatsContainer>
+                        <PageBlock>
+                            <OptionContainer>
+                                <OptionTitle>
+                                    Your public posts
+                                </OptionTitle>
+                                <PageText>
+                                    Here you can view and edit your public posts.
+                                </PageText>
+                                <PageBlock>
+                                    {(loading && !data) || error ? (
+                                        <LoadingContainer>
+                                            <LoadingComponent />
+                                        </LoadingContainer>
+                                    ) : (
+                                        <>
+                                            {data?.postFeed?.length ===
+                                            0 ? (
+                                                <PageText>
+                                                    You have no public posts.
+                                                </PageText>
+                                            ) : (
+                                                <PublicPostsGrid>
+                                                    {data?.postFeed?.map(
+                                                        (post) => (
+                                                            <PostComponent
+                                                                key={
+                                                                    post.id
+                                                                }
+                                                                post={post}
+                                                            />
+                                                        )
+                                                    )}
+                                                </PublicPostsGrid>
+                                            )}
+                                        </>
+                                    )}
+                                </PageBlock>
+                            </OptionContainer>
+                        </PageBlock>
+                    </HomePageContainer>
+                } />
+            } />
         </>
     );
 }
